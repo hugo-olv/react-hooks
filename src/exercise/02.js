883ebc31e30317c682e0de623d40f89840f6fcf5
themaskedcat <oliveira.developpement@gmail.com>
2022-06-06 22:38:28 +0200

    Done 02 Extra 2 - effect dependencies + Test passed

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,6 +8,14 @@ function Greeting({ initialName = '' }) {
     () => window.localStorage.getItem('name') ?? initialName,
   )
 
+  // I went a little too fast over the 2 previous exercises.
+  // and I already added the effect dependencies in the useEffect hook.
+  // So here what it should be for the previous exs :
+  // useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // })
+
+  // And here what it should be now : 
   useEffect(() => {
     window.localStorage.setItem('name', name)
   }, [name])
